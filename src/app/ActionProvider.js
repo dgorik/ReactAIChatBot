2669bb1afedda48dd562c5203fc30117c46a48f6
Daniel Gorbachev <dg186533@gmail.com>
2025-01-19 09:37:52 -0500

    installed open ai library

--- src/app/ActionProvider.js
+++ src/app/ActionProvider.js
@@ -1,6 +1,19 @@
 import OpenAI from 'openai'
 
 const openai = new OpenAI({
-    apiKey: "03a0006726ac4055bad84abb9633c2ef",
-    baseURL: "https://"
+    apiKey: '03a0006726ac4055bad84abb9633c2ef',
+    baseURL: 'https://api.aimlapi.com',
+    dangerouslyAllowBrowser: true,
+
 })
+
+class ActionProvider {
+
+    callGenAI = async(promp) => {
+        constChatCompletion = await openai.chat.completions.create()
+            {
+              model: ''  
+            }
+
+    }
+}
