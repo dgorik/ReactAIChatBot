b0dd1e140dc82202ddda1038bf515412c086a2d2
Daniel Gorbachev <dg186533@gmail.com>
2025-02-07 20:15:13 -0500

    added social media logos

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,15 +1,26 @@
 import React from "react";
 import styles from "./public/Page.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
         <div div className={styles.footer}>
             <div className = {styles.social_media_container}>
-                <a href = "https://x.com/goribaldy19"> Twitter</a>
-                <a className = {styles.medium_anchor} href = "https://www.linkedin.com/in/danielgorbachev1/"> LinkedIn</a>
-                <a href = "https://github.com/dgorik"> Github</a>
+                <div>
+                    <a href="https://github.com/dgorik" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faGithub} style={{color: "black"}} size="2x" />
+                    </a>
+                    <a href="https://x.com/goribaldy19" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faTwitter} style={{color: "black"}} size="2x" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/danielgorbachev1/" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} style={{color: "#0A66C2"}} size="2x" />
+                    </a>
+                </div>
             </div>
         </div>
+            
     )
 }
 
